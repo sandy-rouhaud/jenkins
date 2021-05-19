02b88a47857e0fbc56ded13f9bc15f48a02bb3ae
Sandy <64953937+sandy-rouhaud@users.noreply.github.com>
2021-05-19 15:40:07 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #deploy_remote.sh
-java version
-if [[ $? != 0 ]] then sudo amazon-linux-extras install -y java-openjdk11 fi
-#On copie les fichiers sur la machine distante
-scp /app
-HelloWorld.class
-#On exécute l'application sur la machine distante
-ssh -c 'cd /app && java HelloWorld'
+#!/bin/bash
+#deploy_remote.sh
+
+MACHINE_DESTINATION=ec2-user@ec2-184-72-140-90.compute-1.amazonaws.com
+
+ssh -o StrictHostKeyChecking=no -i /secrets/kp-sandy.pem $MACHINE_DESTINATION 'sudo amazon-linux-extras install -y java-openjdk11'
+
+ssh -o StrictHostKeyChecking=no -i /secrets/kp-sandy.pem $MACHINE_DESTINATION 'sudo mkdir -p /app && sudo chmod -R 777 /app && sudo chown ec2-user /app'
+scp -o StrictHostKeyChecking=no -i /secrets/kp-sandy.pem helloWorld.class $MACHINE_DESTINATION:/home/ec2-user/helloWorld.class
+ssh -o StrictHostKeyChecking=no -i /secrets/kp-sandy.pem $MACHINE_DESTINATION 'sudo cp /home/ec2-user/helloWorld.class /app'
+ssh -o StrictHostKeyChecking=no -i /secrets/sandy.pem $MACHINE_DESTINATION 'sudo cd /app && sudo java helloWorld'
