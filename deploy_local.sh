0dbb5e0982129a429a7d869a9adf66b546016d36
Sandy <64953937+sandy-rouhaud@users.noreply.github.com>
2021-05-19 11:03:47 +0200

    Update deploy_local.sh

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # deploy_local.sh
 pwd
-mkdir -pd /increment02/app
-cp helloWorld.class /increment02/app
-cd /increment02/app
+mkdir -p app
+cp helloWorld.class app
+cd app
 java helloWorld
