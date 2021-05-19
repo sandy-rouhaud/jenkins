2726e4389ae469478d91c4dcb75c854f84417fa2
Sandy <64953937+sandy-rouhaud@users.noreply.github.com>
2021-05-19 15:51:30 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -2,10 +2,11 @@
 #deploy_remote.sh
 
 MACHINE_DESTINATION=ec2-user@ec2-184-72-140-90.compute-1.amazonaws.com
+PATH_PRIVATE_KEY=/secrets/kp-sandy.pem
 
-ssh -o StrictHostKeyChecking=no -i /secrets/kp-sandy.pem $MACHINE_DESTINATION 'sudo amazon-linux-extras install -y java-openjdk11'
+ssh -o StrictHostKeyChecking=no -i $PATH_PRIVATE_KEY $MACHINE_DESTINATION 'sudo amazon-linux-extras install -y java-openjdk11'
 
-ssh -o StrictHostKeyChecking=no -i /secrets/kp-sandy.pem $MACHINE_DESTINATION 'sudo mkdir -p /app && sudo chmod -R 777 /app && sudo chown ec2-user /app'
-scp -o StrictHostKeyChecking=no -i /secrets/kp-sandy.pem helloWorld.class $MACHINE_DESTINATION:/home/ec2-user/helloWorld.class
-ssh -o StrictHostKeyChecking=no -i /secrets/kp-sandy.pem $MACHINE_DESTINATION 'sudo cp /home/ec2-user/helloWorld.class /app'
-ssh -o StrictHostKeyChecking=no -i /secrets/sandy.pem $MACHINE_DESTINATION 'sudo cd /app && sudo java helloWorld'
+ssh -o StrictHostKeyChecking=no -i $PATH_PRIVATE_KEY $MACHINE_DESTINATION 'sudo mkdir -p /app && sudo chmod -R 777 /app && sudo chown ec2-user /app'
+scp -o StrictHostKeyChecking=no -i $PATH_PRIVATE_KEY helloWorld.class $MACHINE_DESTINATION:/home/ec2-user/helloWorld.class
+ssh -o StrictHostKeyChecking=no -i $PATH_PRIVATE_KEY $MACHINE_DESTINATION 'sudo cp /home/ec2-user/helloWorld.class /app'
+ssh -o StrictHostKeyChecking=no -i $PATH_PRIVATE_KEY $MACHINE_DESTINATION 'sudo cd /app && sudo java helloWorld'
